1932ce99a1c62eb667e334d19e6f542424b37d45
Santeri Juhela <santeri.juhela@gmail.com>
2025-07-31 19:03:19 +0300

    1.7: unicafe, step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,12 +6,15 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad }) => (
+const Statistics = ({ good, neutral, bad, all, avg, pos }) => (
   <div>
     <h1>statistics</h1>
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>all {all}</p>
+    <p>average {avg}</p>
+    <p>positive {pos} %</p>
   </div>
 )
 
@@ -21,13 +24,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={() => setGood(good + 1)} text='good' />
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button onClick={() => setBad(bad + 1)} text='bad'/>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        avg={average}
+        pos={positive} />
     </div>
   )
 }
