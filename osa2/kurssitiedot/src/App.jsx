066051c6277335e1dcb6e64456064da86719ac51
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-05 16:45:37 +0300

    2.1: kurssitiedot, step6

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,74 @@
+const Course = ({ course }) => (
+  <>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </>
+)
+
+const Header = ({ name }) => {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => 
+        <Part
+          key={part.id}
+          name={part.name}
+          exercises={part.exercises}
+        />
+      )}
+    </>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
