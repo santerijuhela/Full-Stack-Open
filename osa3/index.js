48675586e2bb8ca46f73ab8ce355dbda6dae1968
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-15 00:28:25 +0300

    3.5: puhelinluettelon backend step5

--- osa3/index.js
+++ osa3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: "1",
@@ -18,7 +20,7 @@ let persons = [
         number: "12-43-234345"
     },
     {
-        id:"4",
+        id: "4",
         name: "Mary Poppendieck",
         number: "39-23-6423122"
     }
@@ -52,6 +54,31 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name) {
+        response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    else if(!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: String(Math.floor(Math.random() * 1000000 + 1))
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
