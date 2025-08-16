a8cac293d3b0b207c78833762eb0e60c97457b1b
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-16 20:54:27 +0300

    3.20: puhelinluettelo ja tietokanta, step8

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
         minlength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: v => /^\d{2,3}-\d{5,}$/.test(v),
+            message: props => `${props.value} is not a valid phone number!`
+        }
+        
+    }
 })
 
 personSchema.set('toJSON', {
