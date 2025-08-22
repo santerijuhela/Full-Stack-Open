e386445d8dd24d89737ad7c283d1f6af432cc654
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-22 19:57:18 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = blogs => {
     return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+    return blogs.reduce((sum, curBlog) => sum + curBlog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
