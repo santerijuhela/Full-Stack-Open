1c69a00fbd9a5ffe7ccbfbe5bc9e6e1045b9b0aa
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-22 19:10:00 +0300

    4.2: blogilista, step2

--- osa4/app.js
+++ osa4/app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const morgan = require('morgan')
+const middleWare = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 const app = express()
+
+morgan.token('body', (request) => {
+    return JSON.stringify(request.body)
+})
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
@@ -16,6 +22,9 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use('/api/blogs', blogsRouter)
+app.use(middleWare.unknownEndpoint)
+app.use(middleWare.errorHandler)
 
 module.exports = app
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -7,12 +7,54 @@ blogsRouter.get('/', (request, response) => {
   })
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id)
+      .then(blog => {
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog.save().then((result) => {
     response.status(201).json(result)
   })
+  .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndDelete(request.params.id)
+      .then(() => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+    const { title, author, url, likes } = request.body
+
+    Blog.findById(request.params.id)
+      .then(blog => {
+        if (!blog) {
+            return response.status(404).end()
+        }
+
+        blog.title = title
+        blog.author = author
+        blog.url = url
+        blog.likes = likes
+
+        return blog.save().then(updatedBlog => {
+            response.json(updatedBlog)
+        })
+      })
+      .catch(error => next(error))
 })
 
 module.exports = blogsRouter
