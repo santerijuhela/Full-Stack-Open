e847657ff8e6b0fd4168a03f00d4bc356b096403
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-22 17:35:37 +0300

    Added router module

--- osa4/index.js
+++ osa4/index.js
@@ -1,7 +1,7 @@
 const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
 const app = express()
@@ -16,20 +16,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
