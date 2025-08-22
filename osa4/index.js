4e177bc8141c84f82f90d3913433fc23105f60a3
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-22 18:06:27 +0300

    Added app.js

--- osa4/index.js
+++ osa4/index.js
@@ -1,22 +1,6 @@
+const app = require('./app')
 const config = require('./utils/config')
-const express = require('express')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-
-const app = express()
-
-logger.info('connecting to', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('error connecting to MongoDB: ', error.message)
-  })
-
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
