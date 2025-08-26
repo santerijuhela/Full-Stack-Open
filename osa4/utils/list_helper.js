ab5e748d715f204151dd5f9433c0571c276fa258
Santeri Juhela <santeri.juhela@gmail.com>
2025-08-26 23:50:03 +0300

    4.7: apufunktioita ja yksikkötestejä, step5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -25,11 +25,27 @@ const mostBlogs = blogs => {
 
     const counted = _.countBy(blogs, 'author')
     mostBloggedAuthor = _.maxBy(Object.keys(counted), author => counted[author])
-    
+
     return {
         author: mostBloggedAuthor,
         blogs: counted[mostBloggedAuthor]
     }
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
+const mostLikes = blogs => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const likesByAuthor = blogs.reduce((result, blog) => {
+        result[blog.author] = (result[blog.author] || 0) + blog.likes
+        return result
+    }, {})
+    const mostLikedAuthor = _.maxBy(Object.keys(likesByAuthor), author => likesByAuthor[author])
+    return {
+        author: mostLikedAuthor,
+        likes: likesByAuthor[mostLikedAuthor]
+    }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
