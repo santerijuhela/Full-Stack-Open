df8d936c6e889cde2a548e2862cd38a4cc8b3e40
Santeri Juhela <santeri.juhela@gmail.com>
2025-09-26 21:42:53 +0300

    5.3: blogilistan frontend, step3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -48,6 +49,11 @@ const App = () => {
     setUser(null)
   }
 
+  const createBlog = async newBlog => {
+    const returnedBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(returnedBlog))
+  }
+
   if (user === null) {
     return (
       <div>
@@ -85,6 +91,9 @@ const App = () => {
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+
+      <h2>create new</h2>
+      <BlogForm handleCreate={createBlog} />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
