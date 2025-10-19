8b128f8c6337464f5779e17f900431d66c3d8cb3
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-19 17:14:28 +0300

    5.14: blogilistan testit, step2

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -8,7 +9,7 @@ describe('<Blog />', () => {
       author: 'Anna Author',
       url: 'https://test.blog.net',
       user: {
-        name: 'Taru tester',
+        name: 'Taru Tester',
         username: 'testUser'
       },
       likes: 23
@@ -31,5 +32,20 @@ describe('<Blog />', () => {
 
     const likeElement = screen.getByText('likes 23')
     expect(likeElement).not.toBeVisible()
+  })
+
+  test('after clicking the button, url and likes are displayed', async () => {
+    const testUser = userEvent.setup()
+    const button = screen.getByText('view')
+    await testUser.click(button)
+
+    const urlElement = screen.getByText('https://test.blog.net')
+    expect(urlElement).toBeVisible()
+
+    const likeElement = screen.getByText('likes 23')
+    expect(likeElement).toBeVisible()
+
+    const userElement = screen.getByText('Taru Tester')
+    expect(userElement).toBeVisible()
   })
 })
