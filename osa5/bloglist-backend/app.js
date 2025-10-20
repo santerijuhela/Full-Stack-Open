8d7c38f357c95fcdc00558d00c66bccecda73d88
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-20 19:41:21 +0300

    5.17: blogilistan end to end -testit, step1

--- osa5/bloglist-backend/app.js
+++ osa5/bloglist-backend/app.js
@@ -29,6 +29,12 @@ app.use(middleWare.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleWare.unknownEndpoint)
 app.use(middleWare.errorHandler)
 
---
+++ osa5/e2e/tests/blog_app.spec.js
@@ -0,0 +1,25 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.goto('/')
+
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByLabel('username')).toBeVisible()
+    await expect(page.getByLabel('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
