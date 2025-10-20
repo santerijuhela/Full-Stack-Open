1d69e9dda325d30cdefc28aa0a31da7d83d8a3ce
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-20 21:49:11 +0300

    5.18: blogilistan end to end -testit, step2

--- osa5/e2e/tests/blog_app.spec.js
+++ osa5/e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -15,11 +16,26 @@ describe('Blog app', () => {
   })
 
   test('Login form is shown', async ({ page }) => {
-    await page.goto('/')
-
     await expect(page.getByText('Log in to application')).toBeVisible()
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'root')
+
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+
+      const notification = page.getByText('Wrong credentials')
+      await expect(notification).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
   })
 })
