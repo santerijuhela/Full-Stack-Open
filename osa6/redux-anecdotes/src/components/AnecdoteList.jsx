6f3a3a86321b0ffd5ad3063f50885462bcd6a1d7
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-28 19:04:22 +0200

    Changed the filtering of anecdotes to use a memoizing selector

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { createSelector } from '@reduxjs/toolkit'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const Anecdote = ({ anecdote, handleClick }) => (
@@ -11,11 +12,21 @@ const Anecdote = ({ anecdote, handleClick }) => (
   </div>
 )
 
+const selectAnecdotes = state => state.anecdotes
+const selectFilter = state => state.filter
+
+const selectFilteredAnecdotes = createSelector(
+  [selectAnecdotes, selectFilter],
+  (anecdotes, filter) => {
+    return anecdotes.filter(anecdote =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  }
+)
+
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(({ anecdotes, filter }) => {
-    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
-  })
+  const anecdotes = useSelector(selectFilteredAnecdotes)
 
   return (
     anecdotes
