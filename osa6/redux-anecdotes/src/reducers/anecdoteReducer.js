524c9be64270980107f3e49d0f57b291d393c04d
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-30 19:19:01 +0200

    6.16 anekdootit ja backend, step3

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -22,5 +23,14 @@ const anecdoteSlice = createSlice({
   }
 })
 
-export const { createAnecdote, voteFor, setAnecdotes } = anecdoteSlice.actions
+const { setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const { createAnecdote, voteFor } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
