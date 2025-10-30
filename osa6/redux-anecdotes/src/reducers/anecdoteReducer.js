10a7daad7eb2cae6a8e261ed148c20a078bcd705
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-30 17:55:15 +0200

    6.15 anekdootit ja backend, step2

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      const anecdote = asObject(content)
-      state.push(anecdote)
+      state.push(action.payload)
     },
     voteFor(state, action) {
       const id = action.payload
--- osa6/redux-anecdotes/src/services/anecdotes.js
+++ osa6/redux-anecdotes/src/services/anecdotes.js
@@ -4,10 +4,24 @@ const getAll = async () => {
   const response = await fetch(baseUrl)
 
   if (!response.ok) {
-    throw new Error('Filed to fetch anecdotes')
+    throw new Error('Failed to fetch anecdotes')
   }
 
-  return response.json()
+  return await response.json()
 }
 
-export default { getAll }
+const createNew = async (content) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content, votes: 0 })
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
+  }
+
+  return await response.json()
+}
+
+export default { getAll, createNew }
