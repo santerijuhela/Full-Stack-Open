2c8a4abe7c22f972f67bf69440eb55cbd784a45a
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-31 20:03:26 +0200

    6.22: react query anecdotes, step3

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,12 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdotes } from './requests'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updatedAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.map(anecdote =>
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      ))
+    }
+  })
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updatedAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1})
   }
 
   const result = useQuery({
@@ -21,7 +32,7 @@ const App = () => {
   if (result.isError) {
     return <div>anecdote service not available due to problems in server</div>
   }
-  
+
   const anecdotes = result.data
 
   return (
--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -25,3 +25,19 @@ export const createAnecdote = async (newAnecdote) => {
 
   return await response.json()
 }
+
+export const updateAnecdote = async (updatedAnecdote) => {
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updatedAnecdote)
+  }
+
+  const response = await fetch(`${baseUrl}/${updatedAnecdote.id}`, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to update anecdote')
+  }
+
+  return await response.json()
+}
