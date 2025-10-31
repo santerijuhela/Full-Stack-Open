108400c6e483a1cdb9b74ddae1232cd90548d5aa
Santeri Juhela <santeri.juhela@gmail.com>
2025-10-31 19:22:38 +0200

    6.21 react query anecdotes, step2

--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -9,3 +9,19 @@ export const getAnecdotes = async () => {
 
   return await response.json()
 }
+
+export const createAnecdote = async (newAnecdote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newAnecdote)
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
+  }
+
+  return await response.json()
+}
