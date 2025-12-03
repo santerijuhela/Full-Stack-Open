d5bb6d2afbd2ce33a52b4680b9872441f9cf2f43
Santeri Juhela <santeri.juhela@gmail.com>
2025-12-03 15:50:05 +0200

    7.5: anekdoottisovellus ja hookit step2

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -82,6 +82,12 @@ const CreateNew = (props) => {
     navigate("/")
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -99,6 +105,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
