7204c4eb9e5a54315821f9cf7babfb816719ecc6
Santeri Juhela <santeri.juhela@gmail.com>
2026-01-03 19:53:47 +0200

    Changed name of the folder

---
+++ osa7/bloglist-redux/bloglist-frontend/src/components/Blog.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const Blog = ({ blog, addLike, user, remove }) => {
+  const [allVisible, setAllVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: allVisible ? "" : "none" };
+  const showForUser = {
+    display:
+      user.name === blog.user.name && user.username === blog.user.username
+        ? ""
+        : "none",
+  };
+
+  const toggleVisibility = () => {
+    setAllVisible(!allVisible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>
+          {allVisible ? "hide" : "view"}
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button onClick={addLike}>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+        <div style={showForUser}>
+          <button onClick={remove}>remove</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
